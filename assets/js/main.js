16a06e373ed03f6af6daf235f1630b69c6c0b346
awrunpatel <arunp3989@gmail.com>
2025-01-24 12:55:40 +0545

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", function () {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+});
+
 $(document).ready(function () {
   $(".navbar .nav-link").on('click', function (event) {
 
